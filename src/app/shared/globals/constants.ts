b2f43a8b625edbb69197c3172b554e021f4e3c68
Djian Lucca <djianlucca@outlook.com>
2018-07-04 12:52:32 -0300

    Arrumando rota

--- src/app/shared/globals/constants.ts
+++ src/app/shared/globals/constants.ts
@@ -1,7 +1,7 @@
 const GOOGLE_MAPS_API_KEY = 'AIzaSyDjBWw7YVFICcBRqOwo5dYXYGPY0ImHtTM';
 const MAP_STYLE = [{"featureType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"lightness":-100}]}];
 
-// const SERVER_URL = 'https://smm-tool-api.herokuapp.com/';
-const SERVER_URL = 'http://localhost:3000/';
+const SERVER_URL = 'https://smm-tool-api.herokuapp.com/';
+// const SERVER_URL = 'http://localhost:3000/';
 
 export { GOOGLE_MAPS_API_KEY, MAP_STYLE, SERVER_URL };
